3c57aebc05c99b234bf9c17cd7c8e5452f496482
5456465tony97202 <109021079@live.asia.edu.tw>
2021-05-07 16:01:58 +0800

    0507

--- demo2.py
+++ demo2.py
@@ -1,5 +1,6 @@
 import sys
 import random as rand
+import numpy as np
 if __name__=="__main__":
     if len(sys.argv)>2:
         seedValu=int(sys.argv[1])
@@ -8,3 +9,12 @@ if __name__=="__main__":
         rand.shuffle(data)
         print("".join(data))
 
+data=list("This is a book")
+np.random.seed(1723)
+rand.shuffle(data)
+print("".join(data))
+
+data=list("This is a book")
+np.random.seed(1723)
+rand.shuffle(data)
+print("".join(data))
